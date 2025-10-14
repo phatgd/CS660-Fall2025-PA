5b5a391391d76f83d4ecc8fb35ed1138112caccf
smgibson19 <smjgibson17@gmail.com>
2025-10-14 12:36:22 -0400

    attempting heappage implementation

--- src/db/HeapPage.cpp
+++ src/db/HeapPage.cpp
@@ -5,29 +5,42 @@
 using namespace db;
 
 HeapPage::HeapPage(Page &page, const TupleDesc &td) : td(td) {
-    // TODO pa1
     // NOTE: header and data should point to locations inside the page buffer. Do not allocate extra memory.
+    
+    // capacity = std::floor((page.size()*8)/(T ∗ 8 + 1)); // number of slots in a page
+    header = &page[0]; // header is begining of page
+    // data = ( P − T ∗ C ); // data is after the header ends
+
 }
 
 size_t HeapPage::begin() const {
     // TODO pa1
-    throw std::runtime_error("not implemented");
+    
+    // loop to find first empty slot
+    // for(int x = 0; x< capacity; x+= size of a slot){
+
+    // }
+    return capacity; // if no empty return end
 }
 
 size_t HeapPage::end() const {
-    // TODO pa1
+    // @author Sam Gibson
+    return capacity;
 }
 
 bool HeapPage::insertTuple(const Tuple &t) {
     // TODO pa1
+    // bitwise or operator?
 }
 
 void HeapPage::deleteTuple(size_t slot) {
     // TODO pa1
+    // clear with bitwise and ~&
 }
 
 Tuple HeapPage::getTuple(size_t slot) const {
     // TODO pa1
+    
 }
 
 void HeapPage::next(size_t &slot) const {
