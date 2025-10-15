818f6abc7bbc0fc55fa7f396e604101edc12b730
smgibson19 <smjgibson17@gmail.com>
2025-10-15 11:47:09 -0400

    working on heappage

--- src/db/HeapPage.cpp
+++ src/db/HeapPage.cpp
@@ -7,24 +7,38 @@ using namespace db;
 
 HeapPage::HeapPage(Page &page, const TupleDesc &td) : td(td) {
     // @author Sam Gibson
-    capacity = std::floor((4096*8)/(static_cast<int>(td.length()) * 8 + 1)); // number of slots in a page
-    header = &page[0]; // header is begining of page
-    data = &page[capacity/8]; // data is after the header ends
 
+    capacity = std::floor((DEFAULT_PAGE_SIZE*8)/(static_cast<int>(td.length()) * 8 + 1)); // number of slots in a page
+    header = page.data(); // header is begining of page
+    data = header + (DEFAULT_PAGE_SIZE - td.length() * capacity); //  P − T ∗ C 
 }
 
 size_t HeapPage::begin() const {
-    // TODO pa1
+    // @author Sam Gibson
     
-    // loop to find first empty slot
-    // for(int x = 0; x< capacity; x+= size of a slot){
-
+    // code TA gave me not working ? 
+    // for(size_t x = *header; x< capacity; x++){ //iterate through header
+    //     if(!empty(x)){ // not empty
+    //         return x;
+    //     }
     // }
-    return capacity; // if no empty return end
+    uint8_t *ptr_first = header; // ptr to head
+    int idx = 0; // index of first slot with data
+
+    for(int x = 0; x< (capacity/8)-1; x++){
+        if(*ptr_first == 1){ // find a full 
+            idx = x;
+            return td.length()*x;
+        }
+        ptr_first++;
+    }
+
+    return capacity; 
 }
 
 size_t HeapPage::end() const {
     // @author Sam Gibson
+
     return capacity;
 }
 
@@ -40,14 +54,30 @@ void HeapPage::deleteTuple(size_t slot) {
 
 Tuple HeapPage::getTuple(size_t slot) const {
     // TODO pa1
-    // use name_to_pos? 
+
+    // ptr + page_size
+    // td.length * capacity
+
+    // data + slot * td.length
+    // deserialize
+
     
 }
 
 void HeapPage::next(size_t &slot) const {
-    // TODO pa1
+    // @author Sam Gibson
+
+    while(++slot < capacity && empty(slot)){ // iterate through slots in header
+    }
+
 }
 
 bool HeapPage::empty(size_t slot) const {
-    // TODO pa1
+     // @author Sam Gibson
+    if(slot == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
