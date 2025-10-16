519ec983fcaff7bfe20879b49205856a0b462551
phatgd <phatduong1011@gmail.com>
2025-10-16 00:02:46 -0400

    insert Tuple maybe

--- src/db/HeapPage.cpp
+++ src/db/HeapPage.cpp
@@ -13,7 +13,7 @@ HeapPage::HeapPage(Page &page, const TupleDesc &td) : td(td) {
     capacity = std::floor((DEFAULT_PAGE_SIZE*8)/(td.length() * 8 + 1)); // number of slots in a page/ header
     header = page.data(); // begining of page
     data = header + (DEFAULT_PAGE_SIZE - td.length() * capacity); //  P − T ∗ C 
-    std::cout << "header is: " << ((header[51/8] >> (7-51%8)) & 1)  << "\n";
+    // std::cout << "header is: " << ((header[51/8] >> (7-51%8)) & 1)  << "\n";
 }
 
 size_t HeapPage::begin() const {
@@ -33,20 +33,29 @@ size_t HeapPage::end() const {
 }
 
 bool HeapPage::insertTuple(const Tuple &t) {
-    // find empty slot
+    // @author Phat Duong
+
+    // find first occupied slot
     size_t slot = begin();
 
-    if (slot == capacity){ // full page
+    // full page
+    if (slot == 0){ 
         return false;
     }
 
+    // get most recent empty slot
+    slot -= 1;
+
     // serialize tuple into data
-    td.serialize(data + slot/8 * td.length(), t);
+    td.serialize((data + slot * td.length()), t);
     
     // mark slot as used in header
-    *(header + slot) = 1;
-
     // bitwise or operator
+    header[slot/8] |= (1 << (7-slot%8));
+
+    // success
+    return true;
+   
 }
 
 void HeapPage::deleteTuple(size_t slot) {
@@ -70,14 +79,12 @@ void HeapPage::next(size_t &slot) const {
     // @author Sam Gibson
 
     while(++slot < capacity && empty(slot)){
-        // std::cout << "slot number: " << slot << "; ";  // iterate through slots in header
     }
-    std::cout << "slot number: " << slot << "; ";  // iterate through slots in header
+    // std::cout << "slot number: " << slot << "; ";  // iterate through slots in header
 
 }
 
 bool HeapPage::empty(size_t slot) const {
-     // @author Sam Gibson, Phat Duong
-    // slot = slot - 1; // adjust for 0 index
+     // @author Phat Duong
     return *header == '\0' || ((header[slot/8] >> (7-slot%8)) & 1) == 0; // check if slot is empty
 }
