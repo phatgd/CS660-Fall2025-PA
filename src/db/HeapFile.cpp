949eb83bc23bfa5248cb9c17e108528f717fd9c9
phatgd <phatduong1011@gmail.com>
2025-10-16 19:37:32 -0400

    Heap File done, all tests passed

--- src/db/HeapFile.cpp
+++ src/db/HeapFile.cpp
@@ -8,15 +8,60 @@ using namespace db;
 HeapFile::HeapFile(const std::string &name, const TupleDesc &td) : DbFile(name, td) {}
 
 void HeapFile::insertTuple(const Tuple &t) {
-    // TODO pa1
+    // @author Phat Duong
+    if(!td.compatible(t)){
+        throw std::invalid_argument("Tuple is not compatible with TupleDesc");
+    }
+
+    Page page;
+    readPage(page, getNumPages()-1); // read last page
+    HeapPage hp(page, td);
+    
+    if (hp.insertTuple(t)){
+        writePage(page, getNumPages()-1); // write back to last page
+        return;
+    }
+
+    // last page is full, create a new page
+    Page new_page = {}; // zero-initialize new page
+    HeapPage new_hp(new_page, td);
+    if (!new_hp.insertTuple(t)){
+        throw std::runtime_error("Failed to insert tuple into new page");
+    }
+
+    writePage(new_page, getNumPages()); // write new page to end of file
+    numPages++; // increment number of pages
 }
 
 void HeapFile::deleteTuple(const Iterator &it) {
-    // TODO pa1
+    // @author Phat Duong
+
+    if(it.page >= getNumPages()){
+        throw std::out_of_range("Iterator out of range of file");
+    }
+
+    Page page;
+    readPage(page, it.page);
+    HeapPage hp(page, td);
+
+    hp.getTuple(it.slot) = Tuple({}); // optional: clear the tuple data
+    hp.deleteTuple(it.slot);
+
+    writePage(page, it.page); // write back to file
 }
 
 Tuple HeapFile::getTuple(const Iterator &it) const {
-    // TODO pa1
+    // @author Phat Duong
+
+    if(it.page >= getNumPages()){
+        throw std::out_of_range("Iterator out of range of file");
+    }
+
+    Page page;
+    readPage(page, it.page);
+    HeapPage hp(page, td);
+
+    return hp.getTuple(it.slot);
 }
 
 void HeapFile::next(Iterator &it) const {
@@ -69,6 +114,8 @@ Iterator HeapFile::begin() const {
             return Iterator(*this, page_num, slot);
         }
     }
+
+    return end(); // no tuples in file
 }
 
 Iterator HeapFile::end() const {
