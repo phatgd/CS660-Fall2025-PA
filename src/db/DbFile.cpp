d55faf03582ce38ced52da10d6fbe43a8844d70c
smgibson19 <smjgibson17@gmail.com>
2025-10-16 00:57:27 -0400

    attempt dbfile?

--- src/db/DbFile.cpp
+++ src/db/DbFile.cpp
@@ -22,7 +22,7 @@ DbFile::DbFile(const std::string &name, const TupleDesc &td) : name(name), td(td
     }
 
     // may need fopen()
-    fd = open(name, 'w+'); // opens/ creates file at that location
+    fd = open(name.c_str(), 2); // opens/ creates file at that location
     if(fd == -1){
         throw std::runtime_error("File can't be opened");
     }
@@ -32,24 +32,41 @@ DbFile::DbFile(const std::string &name, const TupleDesc &td) : name(name), td(td
     if(numPages == 0){ // always has at least one page even if empty
         numPages++;
     }
-
     
 }
 
 DbFile::~DbFile() {
+    // @author Sam Gibson
+
     // TODO pa1: close file
     // Hind: use close
+
+    close(fd);
 }
 
 const std::string &DbFile::getName() const { return name; }
 
 void DbFile::readPage(Page &page, const size_t id) const {
+    // @author Sam Gibson
+
+    void* buf = &page; // void ptr for content
+    ssize_t results = pread(fd, buf, DEFAULT_PAGE_SIZE, id);
+    if(results < 0){
+        throw std::runtime_error("Couldn't read :O");
+    }
+    
     reads.push_back(id);
-    // TODO pa1: read page
-    // Hint: use pread
 }
 
 void DbFile::writePage(const Page &page, const size_t id) const {
+    // @author Sam Gibson
+
+    const void* buf = &page; // void ptr for content
+    ssize_t results = pwrite(fd, buf, DEFAULT_PAGE_SIZE, id);
+    if(results < 0){
+        throw std::runtime_error("Couldn't write :P");
+    }
+    
     writes.push_back(id);
     // TODO pa1: write page
     // Hint: use pwrite
