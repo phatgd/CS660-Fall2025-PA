81112fcfb66e858d89c3bb3dc8f8b413b8f6f0d9
smgibson19 <smjgibson17@gmail.com>
2025-10-27 19:18:28 -0400

    constructor, part of split

--- src/db/LeafPage.cpp
+++ src/db/LeafPage.cpp
@@ -1,10 +1,20 @@
 #include <db/LeafPage.hpp>
 #include <stdexcept>
+#include <cmath>
 
 using namespace db;
 
 LeafPage::LeafPage(Page &page, const TupleDesc &td, size_t key_index) : td(td), key_index(key_index) {
   // TODO pa2
+  // @author Sam Gibson
+
+  // page header is a LeafPageHeader
+  header = reinterpret_cast<LeafPageHeader *>(page.data()); 
+
+  // based on the remaining size of the page and the size of the tuples
+  capacity = (DEFAULT_PAGE_SIZE * 8 - page.size())/(td.length() * 8);
+
+  data = page.data() + DEFAULT_PAGE_SIZE - td.length() * capacity;
 }
 
 bool LeafPage::insertTuple(const Tuple &t) {
@@ -13,8 +23,23 @@ bool LeafPage::insertTuple(const Tuple &t) {
 
 int LeafPage::split(LeafPage &new_page) {
   // TODO pa2
+
+  // calc new size
+  uint16_t origin_size = header -> size;
+
+  this -> header -> size = std::ceil(origin_size/2);
+  new_page.header -> size = std::floor(origin_size/2);
+
+  // resetting next ptrs
+  new_page.header->next_leaf = this -> header -> next_leaf;
+  this -> header -> next_leaf = new_page.key_index;
+  
+  // 
+
+  
 }
 
 Tuple LeafPage::getTuple(size_t slot) const {
   // TODO pa2
+  return td.deserialize(data+slot*td.length());
 }
