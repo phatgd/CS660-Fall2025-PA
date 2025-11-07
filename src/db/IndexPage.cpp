1f1cdf3a871f6ba9480cd7627b8eb30a6493520a
phatgd <phatduong1011@gmail.com>
2025-11-07 16:23:23 -0500

    Fixed index page pointers again

--- src/db/IndexPage.cpp
+++ src/db/IndexPage.cpp
@@ -14,10 +14,10 @@ IndexPage::IndexPage(Page &page) {
 	capacity = DEFAULT_PAGE_SIZE/(sizeof(int) + sizeof(size_t)) - 1;
 
 	// keys pointer starts after header
-	keys = reinterpret_cast<int *>(page.data() + DEFAULT_PAGE_SIZE - (sizeof(int) + sizeof(size_t)) * capacity - sizeof(size_t));
+	keys = reinterpret_cast<int *>(page.data() + sizeof(IndexPageHeader));
 
 	// children pointer starts after keys
-	children = reinterpret_cast<size_t *>(keys + capacity);
+	children = reinterpret_cast<size_t *>(page.data() + sizeof(IndexPageHeader) + capacity * sizeof(int));
 
 	header -> size = (header -> size == '\0') ? 0 : header -> size;
 	// header -> index_children = false; // no index children proabably until after split
@@ -55,18 +55,19 @@ int IndexPage::split(IndexPage &new_page) {
 	int middle = header -> size / 2;
 	
 	// get new headers 
-	header -> size = middle;
 	new_page.header -> size = middle + header -> size % 2 - 1;
+	new_page.header -> index_children = header -> index_children;
+	header -> size = middle;
 	
-
 	// split key data
 	int *key_split = keys + middle + 1;
 	memmove(new_page.keys, key_split, (new_page.header -> size) * (sizeof(int)));
+	std::fill(key_split - 1 , key_split, 0); // optional: clear old keys
 
 	// move children
 	// children size stays the same as keys size + 1
-	size_t *child_split = children + middle + 1;
-	memmove(new_page.children, children, (new_page.header -> size+1) * (sizeof(size_t)));
+	size_t *child_split = children + middle;
+	memmove(new_page.children, child_split, (new_page.header -> size + 1) * (sizeof(size_t)));
 
 	return keys[middle];
 }
