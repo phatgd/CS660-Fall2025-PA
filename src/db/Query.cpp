9625c9048e98c1c3351cb97fe2ee217cd41e70cc
phatgd <phatduong1011@gmail.com>
2025-11-14 18:04:54 -0500

    projection Done

--- src/db/Query.cpp
+++ src/db/Query.cpp
@@ -1,19 +1,36 @@
 #include <db/Query.hpp>
+#include <iostream>
 
 using namespace db;
 
+
+bool operand_check(field_t field1, field_t field2, PredicateOp operation){
+  	// TODO: Implement this
+}
+
 void db::projection(const DbFile &in, DbFile &out, const std::vector<std::string> &field_names) {
-  // TODO: Implement this function
+	// @author Phat Duong, Sam Gibson
+  	std::cout<<"test debugging"<<std::endl;
+  	auto &result_td = in.getTupleDesc();
+	
+	for (const auto &it : in){
+		std::vector<field_t> result_fields{};
+		for (auto &field : field_names){
+			result_fields.push_back(it.get_field(result_td.index_of(field)));
+		}
+		Tuple result_tuple = Tuple(result_fields);
+		out.insertTuple(result_tuple);
+ 	}
 }
 
 void db::filter(const DbFile &in, DbFile &out, const std::vector<FilterPredicate> &pred) {
-  // TODO: Implement this function
+  	// TODO: Implement this function
 }
 
 void db::aggregate(const DbFile &in, DbFile &out, const Aggregate &agg) {
-  // TODO: Implement this function
+  	// TODO: Implement this function
 }
 
 void db::join(const DbFile &left, const DbFile &right, DbFile &out, const JoinPredicate &pred) {
-  // TODO: Implement this function
+  	// TODO: Implement this function
 }
