dd08e0f1dfaa988157aa698063768ba3eda3bd61
smgibson19 <smjgibson17@gmail.com>
2025-11-14 18:22:17 -0500

    projection

--- src/db/Query.cpp
+++ src/db/Query.cpp
@@ -9,6 +9,7 @@ bool operand_check(field_t field1, field_t field2, PredicateOp operation){
 }
 
 void db::projection(const DbFile &in, DbFile &out, const std::vector<std::string> &field_names) {
+<<<<<<< HEAD
 	// @author Phat Duong, Sam Gibson
   	std::cout<<"test debugging"<<std::endl;
   	auto &result_td = in.getTupleDesc();
@@ -21,6 +22,20 @@ void db::projection(const DbFile &in, DbFile &out, const std::vector<std::string
 		Tuple result_tuple = Tuple(result_fields);
 		out.insertTuple(result_tuple);
  	}
+=======
+  //
+  auto &in_td = in.getTupleDesc();
+
+  for(const auto &it : in){
+    std::vector<field_t> result_fields{};
+    for(auto &field: field_names){
+      result_fields.push_back(it.get_field(in_td.index_of(field)));
+    }
+
+    out.insertTuple(Tuple(result_fields));
+    
+  }
+>>>>>>> 49298a3 (projection)
 }
 
 void db::filter(const DbFile &in, DbFile &out, const std::vector<FilterPredicate> &pred) {
